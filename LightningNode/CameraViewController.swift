4b54ef1fc8af88486e05d70e729595fb6f6968ac
Matthew Ramsden <ramsden.matthew@gmail.com>
2019-01-17 22:06:41 -0600

    Camera updates

--- LightningNode/CameraViewController.swift
+++ LightningNode/CameraViewController.swift
@@ -9,27 +9,73 @@
 import UIKit
 import AVFoundation
 
-public class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController {
+    
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
     
-    override public func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .black
+        setupCaptureSession()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        let button = UIButton(frame: CGRect(x: 10, y: 60, width: 40, height: 40))
-        button.backgroundColor = .clear
-        button.setTitle("<", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        self.view.addSubview(button)
+        if (self.captureSession?.isRunning == false) {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (self.captureSession?.isRunning == true) {
+            self.captureSession.stopRunning()
+        }
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+}
 
+extension CameraViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let readableString = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            let url = URL(string: readableString)
+            let queryParameters = url!.queryParameters
+            let certificate = queryParameters!["cert"]?.base64UrlToBase64()
+            let macaroonString = queryParameters?["macaroon"]?.base64UrlToBase64()
+            let nodeHostString = url?.host
+            let port = url?.port
+            let nodeString = "\(nodeHostString!):\(port!)"
+            let rnc = RemoteNodeConnection.init(uri: nodeString, certificate: certificate!, macaroon: macaroonString!)
+            let bundle = Bundle(for: AddNodeViewController.self)
+            let addNodeIdentifier = Reusing<AddNodeViewController>().identifier()
+            let storyboard = UIStoryboard(name: addNodeIdentifier, bundle: bundle)
+            let vc = storyboard.instantiateViewController(withIdentifier: addNodeIdentifier) as! AddNodeViewController
+            vc.remoteNodeConnection = rnc
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        
+        dismiss(animated: true)
+    }
+}
+
+extension CameraViewController {
+    func setupCaptureSession() {
         self.captureSession = AVCaptureSession()
         
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-        let videoInput: AVCaptureDeviceInput // (device: captureDevice) other has this
-        
+        let videoInput: AVCaptureDeviceInput
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
@@ -44,10 +90,8 @@ public class CameraViewController: UIViewController {
         }
         
         let metadataOutput = AVCaptureMetadataOutput()
-        
         if (self.captureSession.canAddOutput(metadataOutput)) {
             self.captureSession.addOutput(metadataOutput)
-            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.qr]
         } else {
@@ -60,23 +104,13 @@ public class CameraViewController: UIViewController {
         self.previewLayer.videoGravity = .resizeAspectFill
         self.view.layer.addSublayer(self.previewLayer)
         
+        let button = UIButton(frame: CGRect(x: 10, y: 60, width: 40, height: 40))
+        button.backgroundColor = .clear
+        button.setTitle("<", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
         self.captureSession.startRunning()
-    }
-    
-    override public func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if (self.captureSession?.isRunning == false) {
-            self.captureSession.startRunning()
-        }
-    }
-    
-    override public func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        if (self.captureSession?.isRunning == true) {
-            self.captureSession.stopRunning()
-        }
     }
     
     func deviceInputFail() {
@@ -88,50 +122,4 @@ public class CameraViewController: UIViewController {
         present(alertController, animated: true)
         self.captureSession = nil
     }
-    
-    func captured(qr: String) {
-        
-        let url = URL(string: qr)
-        let queryParameters = url!.queryParameters
-        
-        let certificate = queryParameters!["cert"]?.base64UrlToBase64()
-        
-        let macaroonString = queryParameters?["macaroon"]?.base64UrlToBase64()
-        
-        let nodeHostString = url?.host
-        let port = url?.port
-        let nodeString = "\(nodeHostString!):\(port!)"
-        
-        let rnc = RemoteNodeConnection.init(uri: nodeString, certificate: certificate!, macaroon: macaroonString!)
-        
-        let bundle = Bundle(for: AddNodeViewController.self)
-        let addNodeIdentifier = Reusing<AddNodeViewController>().identifier()
-        let storyboard = UIStoryboard(name: addNodeIdentifier, bundle: bundle)
-        let vc = storyboard.instantiateViewController(withIdentifier: addNodeIdentifier) as! AddNodeViewController
-        vc.remoteNodeConnection = rnc
-        self.navigationController?.pushViewController(vc, animated: true)
-
-    }
-    
-    @objc func buttonAction(sender: UIButton!) {
-        _ = navigationController?.popViewController(animated: true)
-    }
-    
-}
-
-extension CameraViewController: AVCaptureMetadataOutputObjectsDelegate {
-    
-    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        captureSession.stopRunning()
-        
-        if let metadataObject = metadataObjects.first {
-            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-            guard let readableString = readableObject.stringValue else { return }
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            captured(qr: readableString)
-        }
-        
-        dismiss(animated: true)
-    }
-    
 }
