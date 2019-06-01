9dde8e27238293bd8de5c2020f1e5a358ecd879a
Matthew <6657488+reez@users.noreply.github.com>
2019-06-01 15:37:52 -0500

    notification: add local notification and background fetch

--- LightningNode/AppDelegate.swift
+++ LightningNode/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,11 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        UIApplication.shared.setMinimumBackgroundFetchInterval(1800) // 30 mins
+        registerForLocalNotifications(application: application)
+        
         let vc = AddNodeViewController()
         let navigationController = UINavigationController(rootViewController: vc)
         navigationController.navigationBar.isHidden = true
         navigationController.view.backgroundColor = .white
-
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
@@ -26,4 +30,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    func registerForLocalNotifications(application: UIApplication) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [ .alert]) { [weak center, weak self] (granted, error) in
+            guard granted, let center = center, let `self` = self else { return }
+            print("registerForLocalNotifications")
+        }
+    }
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        switch Current.keychain.load() {
+        case let .success(savedConfig):
+            
+            Current.remoteNodeConnectionFormatted = savedConfig
+            Current.lightningAPIRPC.info { [weak self] result in
+                try? result.get()
+                    |> flatMap {
+
+                        if $0.syncedToChain == false {
+                            self?.addNotification(syncStatus: $0.syncedToChain)
+                        }
+                        
+                        completionHandler(.newData)
+                }
+            }
+        case .failure(_):
+            completionHandler(.failed)
+        }
+        
+    }
+    
+    func addNotification(syncStatus: Bool) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "🌩 Node is Not Synced"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let identifier = UUID().uuidString
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+            if let error = error {
+                print(error)
+            }
+            print("added(request)")
+        })
+    }
+    
+
 }
