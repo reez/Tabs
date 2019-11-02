a37eae7942fc04df70f9863fda73622a472aa531
Matthew Ramsden <ramsden.matthew@gmail.com>
2019-11-02 14:36:15 -0500

    status detail

--- LightningNode/StatusDetailUIView.swift
+++ LightningNode/StatusDetailUIView.swift
@@ -8,16 +8,67 @@
 
 import SwiftUI
 
-class StatusDetailAppState: ObservableObject {
-    @Published var infoLabel = "Getting Info"
+class TabsAppState: ObservableObject {
+//    @Published var lightningNode: LightningViewModel!
+    @Published var alias = ""
+    @Published var blockHeight = ""
+    @Published var chainsArray = ""
+    @Published var network = ""
+    @Published var numActiveChannels = ""
+    @Published var numPeers = ""
+    @Published var numPendingChannels = ""
+    @Published var syncedToChain = ""
 }
 
+//class StatusDetailAppState: ObservableObject {
+//    @Published var infoLabel = "Getting Info..."
+//}
+
 struct StatusDetailUIView: View {
-    @ObservedObject var state = StatusDetailAppState()
+//    @ObservedObject var state = StatusDetailAppState()
+    @ObservedObject var appState = TabsAppState()
+
+    var body: some View {
+                
+        Group {
+            Text("alias: ")
+                .bold()
+                + Text(self.appState.alias)
+            
+            Text("chainsArray: ")
+                .bold()
+                + Text(self.appState.chainsArray)
+
+            Text("network: ")
+                .bold()
+                + Text(self.appState.network)
+            
+            Text("numActiveChannels: ")
+                .bold()
+                + Text("\(self.appState.numActiveChannels)")
+            
+            Text("numPeers: ")
+                .bold()
+                + Text("\(self.appState.numPeers)")
+            
+            Text("numPendingChannels: ")
+                .bold()
+                + Text("\(self.appState.numPendingChannels)")
+            
+            Text("syncedToChain: ")
+                .bold()
+                + Text(String(self.appState.syncedToChain))
+            
+        }
+        .font(.caption)
+        .onAppear { self.loadStatusDetail() }
+        
+    }
     
-    
+}
+
+extension StatusDetailUIView {
     func loadStatusDetail() {
-        //        self.nvActivityIndicator?.startAnimating()
         switch Current.keychain.load() {
         case let .success(savedConfig):
             
@@ -27,28 +78,37 @@ struct StatusDetailUIView: View {
             Current.lightningAPIRPC.info { result in // [weak self]
                 try? result.get()
                     |> flatMap {
-                        //                        self?.viewModel.lightningNodeInfo = $0
+                        // self?.viewModel.lightningNodeInfo = $0
                         
-                        let text = """
-                        "alias": \($0.alias)
+                        //                        let text = """
+                        //                        "alias": \($0.alias)
+                        //
+                        //                        "blockHeight": \($0.blockHeight)
+                        //
+                        //                        "chain": \($0.chainsArray)
+                        //
+                        //                        "network": \($0.network)
+                        //
+                        //                        "numActiveChannels": \($0.numActiveChannels)
+                        //
+                        //                        "numPeers": \($0.numPeers)
+                        //
+                        //                        "numPendingChannels": \($0.numPendingChannels)
+                        //
+                        //                        "syncedToChain": \($0.syncedToChain)
+                        //                        """
+                        //
+                        //                        self.state.infoLabel = text
                         
-                        "blockHeight": \($0.blockHeight)
+                        self.appState.alias = $0.alias
+                        self.appState.blockHeight = String($0.blockHeight)
+                        self.appState.chainsArray = $0.chainsArray
+                        self.appState.network = $0.network
+                        self.appState.numActiveChannels = String($0.numActiveChannels)
+                        self.appState.numPeers = String($0.numPeers)
+                        self.appState.numPendingChannels = String($0.numPendingChannels)
+                        self.appState.syncedToChain = String($0.syncedToChain)
                         
-                        "chain": \($0.chainsArray)
-                        
-                        "network": \($0.network)
-                        
-                        "numActiveChannels": \($0.numActiveChannels)
-                        
-                        "numPeers": \($0.numPeers)
-                        
-                        "numPendingChannels": \($0.numPendingChannels)
-                        
-                        "syncedToChain": \($0.syncedToChain)
-                        """
-                        
-                        self.state.infoLabel = text
-                        //                        self?.nvActivityIndicator?.stopAnimating()
                 }
             }
         case .failure(_):
@@ -63,19 +123,6 @@ struct StatusDetailUIView: View {
             //            }
         }
     }
-    
-    var body: some View {
-        
-        VStack {
-            Text(state.infoLabel)
-                .font(.caption)
-        }
-        .onAppear {
-            self.loadStatusDetail()
-        }
-        
-    }
-    
 }
 
 struct StatusDetailUIView_Previews: PreviewProvider {
