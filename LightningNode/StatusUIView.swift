1f8226b97e517d972dfaa21a628fe44d2e85ff9e
Matthew Ramsden <ramsden.matthew@gmail.com>
2019-11-02 14:01:39 -0500

    status

--- LightningNode/StatusUIView.swift
+++ LightningNode/StatusUIView.swift
@@ -7,9 +7,12 @@
 //
 
 import SwiftUI
+import M13Checkbox
 
 class StatusAppState: ObservableObject {
     @Published var syncedLabel = "Syncing..."
+    @Published var syncedImage = "checkmark.circle"
+    @Published var syncedState = false
     @Published var refreshedLabel = "Refreshing..."
 }
 
@@ -34,10 +37,17 @@ struct StatusUIView: View {
                         let formattedDate = mrDateFormatter.string(from: creationDate)
                         self.state.refreshedLabel = "Refreshed: \(formattedDate)"
                         
-                        //                        $0.syncedToChain ?
-                        //                            (self?.checkbox.setCheckState(.checked, animated: true)) :
-                        //                            (self?.checkbox.setCheckState(.unchecked, animated: true))
+                        $0.syncedToChain ?
+                            (self.state.syncedImage = "checkmark.circle") :
+                            (self.state.syncedImage = "xmark.circle")
                         
+                        $0.syncedToChain ?
+                            (self.state.syncedState = true) :
+                            (self.state.syncedState = false)
+                        
+                        print("s2c: \($0.syncedToChain)")
+                        print("sS: \(self.state.syncedState)")
+
                         $0.syncedToChain ?
                             (self.state.syncedLabel = "Synced") :
                             (self.state.syncedLabel = "Not Synced")
@@ -45,6 +55,7 @@ struct StatusUIView: View {
             }
         case .failure(_):
             print("yoyo")
+            
             //            if (self.navigationController != nil) {
             //                print("self.navigationController != nil")
             //                self.navigationController?.popToRootViewController(animated: true)
@@ -69,20 +80,17 @@ struct StatusUIView: View {
             Image(systemName: "checkmark.circle")
                 .resizable()
                 .frame(width: 175, height: 175, alignment: .center)
-                .foregroundColor(.green)
+                .foregroundColor(.blue)
+//                .foregroundColor(state.syncedState ? .green : .red)
             
             Button("Get Info") {
-                print("Get it")
                 self.showAlert = true
-                
             }
             .padding()
             .foregroundColor(.blue)
             .padding()
             .sheet(isPresented: $showAlert, onDismiss: {
-                print("Dismissed")
                 self.showAlert = false
-                
             }) {
                 StatusDetailUIView()
             }
