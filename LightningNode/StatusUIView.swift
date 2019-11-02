1bfcea589ace3510cb2129a4790093308a7314d9
Matthew Ramsden <ramsden.matthew@gmail.com>
2019-11-02 14:06:46 -0500

    status2

--- LightningNode/StatusUIView.swift
+++ LightningNode/StatusUIView.swift
@@ -7,11 +7,10 @@
 //
 
 import SwiftUI
-import M13Checkbox
 
 class StatusAppState: ObservableObject {
     @Published var syncedLabel = "Syncing..."
-    @Published var syncedImage = "checkmark.circle"
+    @Published var syncedImage = "xmark.circle"
     @Published var syncedState = false
     @Published var refreshedLabel = "Refreshing..."
 }
@@ -20,6 +19,39 @@ struct StatusUIView: View {
     @ObservedObject var state = StatusAppState()
     @State private var showAlert = false
     
+    var body: some View {
+        
+        VStack {
+            
+            Text(self.state.syncedLabel)
+                .font(.headline)
+            
+            Text(self.state.refreshedLabel)
+                .font(.footnote)
+            
+            Image(systemName: self.state.syncedImage)
+                .resizable()
+                .frame(width: 175, height: 175, alignment: .center)
+                .foregroundColor(.blue) //.foregroundColor(state.syncedState ? .green : .red)
+            
+            Button("Get Info") { self.showAlert = true }
+                .padding()
+                .foregroundColor(.blue)
+                .padding()
+                .sheet(
+                    isPresented: $showAlert,
+                    onDismiss: { self.showAlert = false })
+                    { StatusDetailUIView() }
+            
+        }.onAppear {
+            self.loadStatus()
+        }
+        
+    }
+    
+}
+
+extension StatusUIView {
     func loadStatus() {
         switch Current.keychain.load() {
         case let .success(savedConfig):
@@ -41,21 +73,22 @@ struct StatusUIView: View {
                             (self.state.syncedImage = "checkmark.circle") :
                             (self.state.syncedImage = "xmark.circle")
                         
+                        print("syncedIM: \(self.state.syncedImage)")
+                        
                         $0.syncedToChain ?
                             (self.state.syncedState = true) :
                             (self.state.syncedState = false)
                         
                         print("s2c: \($0.syncedToChain)")
                         print("sS: \(self.state.syncedState)")
-
+                        
                         $0.syncedToChain ?
                             (self.state.syncedLabel = "Synced") :
                             (self.state.syncedLabel = "Not Synced")
                 }
             }
         case .failure(_):
-            print("yoyo")
-            
+            print("Status fetch failure")
             //            if (self.navigationController != nil) {
             //                print("self.navigationController != nil")
             //                self.navigationController?.popToRootViewController(animated: true)
@@ -65,43 +98,6 @@ struct StatusUIView: View {
             //            }
         }
     }
-    
-    var body: some View {
-        
-        VStack {
-            
-            Text(self.state.syncedLabel)
-                .font(.headline)
-            
-            // $ ?
-            Text(self.state.refreshedLabel)
-                .font(.footnote)
-            
-            Image(systemName: "checkmark.circle")
-                .resizable()
-                .frame(width: 175, height: 175, alignment: .center)
-                .foregroundColor(.blue)
-//                .foregroundColor(state.syncedState ? .green : .red)
-            
-            Button("Get Info") {
-                self.showAlert = true
-            }
-            .padding()
-            .foregroundColor(.blue)
-            .padding()
-            .sheet(isPresented: $showAlert, onDismiss: {
-                self.showAlert = false
-            }) {
-                StatusDetailUIView()
-            }
-            
-        }.onAppear {
-            self.loadStatus()
-        }
-        
-        
-    }
-    
 }
 
 struct StatusUIView_Previews: PreviewProvider {
